d5f4e88921bca18db6b5e9a66ec94f0771bcc95d
Noah Amsel <ebminmaj75@gmail.com>
2018-11-27 23:02:02 -0500

    fucked arounnnnndd

---
+++ process_nex.R
@@ -0,0 +1,13 @@
+library(lattice)
+library(ape)
+library(geiger)
+library(phangorn)
+library(phytools)
+library(phyloclim)
+
+turk <- read.nexus.data(file = "data/Turkic12langs.nex")
+turk.phydata <- phyDat(as.data.frame(turk), format="NEXUS", type="USER", levels=c("0", "1"))
+#print(as.data.frame(turk))
+dm1 <- dist.ml(turk.phydata, method="maximum")
+dm2 <- dist(t(as.matrix(as.data.frame(lapply(turk, as.numeric)))), method="euclidean")
+
